e0970a93a470ff3554eb6e4797eb90416499e715
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-03 17:43:04 +0600

    chef details added

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import { FaBrain, FaHamburger, FaThumbsUp } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
+import RecipesDetails from './RecipesDetails';
 
 const Recipe = () => {
 
     const singleData = useLoaderData()
-    const {name, experience, likes,  main_recipes, picture, recipes} = singleData
+    const { name, experience, likes, main_recipes, picture, recipes, bio } = singleData
 
     return (
         <div className='background'>
-                    s
+            <div className='container mx-auto grid grid-cols-12 gap-12 items-center'>
+                <div className=' backdrop-blur-xl w-fit  py-5 px-8 flex flex-col col-span-4 rounded-md'>
+                    <div className="avatar">
+                        <div className="w-72 mask mask-squircle mx-auto">
+                            <img src={picture} />
+                        </div>
+                    </div>
+                    <div className='text-accent space-y-3 mt-3'>
+                        <h1 className=' text-3xl font-bold'>{name}</h1>
+                        
+                        <p className='font-normal text-sm opacity-80 flex items-center gap-2'>{bio}</p>
+                        <p className='font-normal flex items-center gap-2'><FaBrain /> Experience : {experience}years</p>
+                        <p className='font-normal flex items-center gap-2'><FaHamburger />Recipes : {recipes}</p>
+                        <p className='font-normal flex items-center gap-2'><FaThumbsUp />Likes : {likes}</p>
+                    </div>
+                </div>
+                <div className='col-span-8 '>
+                    {
+                        main_recipes.map((recipes,i) => <RecipesDetails recipes={recipes} key={i}></RecipesDetails>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/RecipesDetails.jsx
+++ src/components/RecipesDetails.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const RecipesDetails = () => {
+const RecipesDetails = ({ recipes }) => {
+    console.log(recipes);
+
+    const { name, cuisine, ingredients, cooking_method, image } = recipes;
+
     return (
         <div>
-            recipes details here
+            <div className="card lg:card-side backdrop-blur-lg shadow-xl rounded-none w-4/5 mx-auto">
+                <figure>
+                    <div className="avatar ">
+                        <div className="w-60 mask mask-hexagon ">
+                            <img src={image} />
+                        </div>
+                    </div>
+                </figure>
+                <div className="card-body text-warning">
+                    <h2 className="card-title">New album is released!</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-secondary">Listen</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
