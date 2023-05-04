fb194f7c2be7c38c1ddfc1bd1d86a549542ba156
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-04 13:18:30 +0600

    Google registration and login done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Register = () => {
+    const navigate = useNavigate()
+    // context api 
+    const { handleGoogleSignIn } = useContext(AuthContext)
+
+    // error 
+    const [error, setError] = useState('')
+
+    // success 
+    const [success, setSuccess] = useState('')
+
+
+    // register with google 
+    const googleSignIn = () => {
+        setError('')
+        setSuccess('')
+        handleGoogleSignIn()
+            .then(result => {
+                const loggedUser = result.user
+                console.log(loggedUser)
+                setSuccess('Account registered successfully')
+                navigate('/home')
+            })
+            .catch(error => setError(error.message))
+    }
+
+
+    // form submit  
+    const handleRegistration = event => {
+        event.preventDefault()
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+        const name = form.name.value
+        const photo = form.photo.value
+
+
+        // password validation 
+        setError('')
+        setSuccess('')
+
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Please provide one upperCase word')
+            return
+        }
+        else if (!/(?=.*[0-9])/.test(password)) {
+            setError('Please provide at least one number')
+            return
+        }
+        else if (password.length < 6) {
+            setError('Password must be 6 characters or above')
+            return
+        }
+
+        setSuccess('Account registered successfully')
+        form.reset()
+
+    }
+
+
     return (
         <div className="flex justify-center items-center h-screen background ">
-            <form className="backdrop-blur-xl  shadow-md rounded px-14 pt-14 pb-6 space-y-5 mb-4" >
+            <form className="backdrop-blur-xl  shadow-md rounded px-14 pt-14 pb-6 space-y-5 mb-4" onSubmit={handleRegistration}>
                 <div className="mb-4">
                     <label className="block text-white  font-bold mb-2 text-xl" htmlFor="email">
                         Name
@@ -26,6 +87,7 @@ const Register = () => {
                         type="text"
                         name='email'
                         placeholder="Email"
+                        required
                     />
                 </div>
                 <div className="mb-6">
@@ -38,6 +100,7 @@ const Register = () => {
                         type="password"
                         name='password'
                         placeholder="Password"
+                        required
                     />
                 </div>
                 <div className="mb-6">
@@ -58,6 +121,26 @@ const Register = () => {
 
                     >
                         Sign Up
+                    </button>
+                </div>
+                <p className='text-red-400 '>{error}</p>
+                <p className='text-green-500 '>{success}</p>
+                <div className='flex flex-col gap-2'>
+                    <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
+ hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100" type='button' onClick={googleSignIn}>
+                        <div className="relative flex items-center space-x-4 justify-center">
+                            <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" className="absolute left-0 w-5" alt="google logo" />
+                            <span className="block w-max font-semibold tracking-wide text-slate-400  text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">Continue with Google</span>
+                        </div>
+                    </button>
+                    <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
+ hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100" type='button'>
+                        <div className="relative flex items-center space-x-4 justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="absolute left-0 w-5 text-gray-700" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                            </svg>
+                            <span className="block w-max font-semibold tracking-wide text-slate-400 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">Continue with Github</span>
+                        </div>
                     </button>
                 </div>
                 <div className="mt-5 text-sm">
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,18 +1,40 @@
 import React, { createContext } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from '../firebase.config';
+
+
 
 export const AuthContext = createContext(null)
+const auth = getAuth(app);
 
 
-const AuthProvider = ({children}) => {
+
+
+
+
+const AuthProvider = ({ children }) => {
+
     const user = 'Rakib Khan'
 
+
+
+    // google signIn 
+    const googleProvider = new GoogleAuthProvider()
+    const handleGoogleSignIn = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+
+    
+    // auth information 
     const authInfo = {
-        user
+        user,
+        handleGoogleSignIn
     }
 
     return (
         <AuthContext.Provider value={authInfo}>
-            {children}  
+            {children}
         </AuthContext.Provider>
     );
 };
